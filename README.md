d7018abc0175886f43b8da75d064d983eab7e196
Mark Scholtz <mark.scholtz@unboxedconsulting.com>
2011-05-15 09:21:28 +0200

    Symlink rvm hook files

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ Setup
 Make the install file executable
   <pre>chmod 755 install.rb</pre>
 
-Symlink the dotfiles to your home (~) directory by running the install file (which also sets up git post-commit hooks to push automatically on commit)
+Run the install script which does the following:
+* 1) Symlink the dotfiles to your home (~) directory
+* 2) Symlink the rvm hook files to your rvm hooks directory (~/.rvm/hooks) directory
+* 3) Set up git post-commit hooks to push automatically on commit
   <pre>`./install.rb`</pre>
--- install.rb
+++ install.rb
@@ -1,15 +1,28 @@
 #!/usr/bin/env ruby
 # from http://errtheblog.com/posts/89-huba-huba
 
-home = File.expand_path('~')
 
+# install dotfiles
+puts '1) Installing dot files ...'
+home_directory = File.expand_path('~')
 Dir['*'].each do |file|
-  next if file =~ /install|readme/i
-  target = File.join(home, ".#{file}")
-  puts "ln -svf #{File.expand_path file} #{target}"
+  next if file =~ /install|readme|rvmhooks/i
+  target = File.join(home_directory, ".#{file}")
+  puts "---> ln -svf #{File.expand_path file} #{target}"
+  `ln -svf #{File.expand_path file} #{target}`
+end
+
+# install rvm hook files
+puts "\n2) Installing rvm hook files ..."
+rvmhooks_directory = File.join(home_directory, '.rvm/hooks')
+myrvmhookfiles = File.join('**', 'rvmhooks', '*')
+Dir[myrvmhookfiles].each do |file|
+  target = File.join(rvmhooks_directory, "#{file.split('/')[1]}")
+  puts "---> ln -svf #{File.expand_path file} #{target}"
   `ln -svf #{File.expand_path file} #{target}`
 end
 
 # git push on commit
+puts "\n3) Setting up git post-commit hooks"
 `echo 'git push' > .git/hooks/post-commit`
 `chmod 755 .git/hooks/post-commit`
